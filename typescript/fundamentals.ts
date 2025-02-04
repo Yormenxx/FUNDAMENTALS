6ca91035eabe2569c748dfa00b0faf3abef7770f
Juan Ortiz <105932267+Yormenxx@users.noreply.github.com>
2025-02-03 20:22:07 -0500

    Update fundamentals.ts

--- typescript/fundamentals.ts
+++ typescript/fundamentals.ts
@@ -1,1 +1,31 @@
+type Person={
+    name :string
+}
+
+const persona :Person = {
+    name: "juan",
+}
+
+console.log(persona.name);
+//string
+let surname : string;
+let person2 : string = "ana";
+//number
+let favNumber : number;
+let favNumber2 : number  = 2;
+//boolean
+let isHard : boolean = false;
+
+
+//Inferencia de tipos
+let tech = "typescript";
+let favNumber3  =6;
+let isHard2 = true;
+
+console.log(typeof(tech));
+console.log(typeof(favNumber3));
+console.log(typeof(isHard2));
+
+
+
 
