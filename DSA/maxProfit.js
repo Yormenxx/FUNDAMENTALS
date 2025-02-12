30d565d5eadb7bfb095ba78866696bc3b7980c0e
Juan Ortiz <105932267+Yormenxx@users.noreply.github.com>
2025-02-11 21:05:37 -0500

    Add files via upload

---
+++ DSA/maxProfit.js
@@ -0,0 +1,27 @@
+const maxProfit= prices =>{
+
+    let minxPrice = prices[0]
+
+    let maxProfit = 0
+
+    for (let i = 1; i < prices.length; i++) {
+        
+        const currentPrice = prices[i]
+        
+        
+    }
+
+
+
+
+
+
+
+
+}
+
+const prices =  [7,1,5,3,6,4]
+
+const profit = maxProfit(prices)
+
+console.log("Max profit", profit)
